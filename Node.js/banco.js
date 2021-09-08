cd69ef59fbf8ba474b140e254b5d930b8cece62b
amanda-laryssa <amanda.laryssa@academico.ifpb.edu.br>
2021-09-08 09:11:03 -0300

    Create banco.js

---
+++ Node.js/banco.js
@@ -0,0 +1,7 @@
+const mysql                 = require("mysql");
+const path                  = require("path");
+
+host     : "localhost",
+user     : "administrator",
+password : "Admin@123",
+database : "easy_control"
